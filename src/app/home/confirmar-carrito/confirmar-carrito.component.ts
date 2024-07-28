85c58ea27a6c64f6042a80fe23c960c90cc76522
KenayOG <angel1202a@gmail.com>
2024-07-27 22:49:01 -0600

    Cambios de prueba por error

---
+++ src/app/home/confirmar-carrito/confirmar-carrito.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmar-carrito',
+  templateUrl: './confirmar-carrito.component.html',
+  styleUrl: './confirmar-carrito.component.css'
+})
+export class ConfirmarCarritoComponent {
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductosHomeComponent } from './productos-home/productos-home.component';
+import { ConfirmarCarritoComponent } from './confirmar-carrito/confirmar-carrito.component';
+import { RouterLink, RouterModule, Routes } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { OrderListModule } from 'primeng/orderlist';
+import { NgbPopoverConfig, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    ProductosHomeComponent
+  declarations: [ProductosHomeComponent, ConfirmarCarritoComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterLink,
+    CardModule,
+    ButtonModule,
+    OrderListModule,
+    NgbPopoverModule,
   ],
-  imports: [CommonModule],
 })
-export class HomeModule {}
+export class HomeModule {
+  constructor(config: NgbPopoverConfig) {
+    config.placement = 'end';
+    config.triggers = 'hover';
+
+    config.popperOptions = (options) => {
+      for (const modifier of options.modifiers || []) {
+        if (modifier.name === 'offset' && modifier.options) {
+          modifier.options['offset'] = () => [30, 8];
+        }
+      }
+      return options;
+    };
+  }
+}
